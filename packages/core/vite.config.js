14d9563fe516ff572e0f679253c344621ced6d55
tseneviratne <tom@blueorange.ai>
2024-08-19 16:44:46 +1000

    Attempting to package correctly for importing into other projects.

--- packages/core/vite.config.js
+++ packages/core/vite.config.js
@@ -5,7 +5,7 @@ import dts from 'vite-plugin-dts';
 export default defineConfig ({
     build: {
         lib: {
-            entry: resolve(__dirname, "src/vite-entry.tsx"),
+            entry: resolve(__dirname, "src/index.ts"),
             name: "core",
             fileName: "index",
         },
@@ -16,8 +16,7 @@ export default defineConfig ({
     plugins: [
         dts({
             insertTypesEntry: true,
-            outputDir: "dist/types",
-            entryRoot: "src",
+            entryRoot: "src/index.ts",
         }),
     ],
 });
