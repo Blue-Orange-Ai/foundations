7ba153447e97ca7fc9f66e0b05a94c0b66675010
tseneviratne <tom@blueorange.ai>
2024-09-11 08:51:19 +1000

    Updated the sidebar body item to allow a click event across the whole div with an exclusion on the right side.

--- packages/core/src/components/layouts/sidebar/items/sidebar-body-item/SideBarBodyItem.tsx
+++ packages/core/src/components/layouts/sidebar/items/sidebar-body-item/SideBarBodyItem.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useRef, useState} from "react";
 
 import './SideBarBodyItem.css'
 
@@ -33,10 +33,34 @@ export const SideBarBodyItem: React.FC<Props> = ({
 													 hoverItems,
 													 onClick}) => {
 
+	const rightSideRef = useRef<HTMLDivElement | null>(null);
+
+	const isDescendantOf = (parent:HTMLElement | null, child:HTMLElement | null) =>{
+		if (parent && child) {
+			if (parent === child) {
+				return child
+			}
+			try{
+				var node = child.parentElement;
+				while (node != null){
+					if (node === parent){
+						return node;
+					}
+					node = node.parentElement;
+				}
+				return null;
+			} catch (e) {
+				return null;
+			}
+		}
+		return null;
+	}
+
 	const [isHovered, setIsHovered] = useState(false);
 
-	const itemClicked = () => {
-		if (onClick) {
+	const itemClicked = (ev: any) => {
+		const target = ev.target as HTMLElement;
+		if (onClick && !isDescendantOf(rightSideRef.current, target)) {
 			onClick()
 		}
 	}
@@ -54,15 +78,15 @@ export const SideBarBodyItem: React.FC<Props> = ({
 			style={active ? activeStyle : style}
 			onMouseEnter={mouseEntered}
 			onMouseLeave={mouseLeave}
+			onClick={itemClicked}
 			className={active ? "blue-orange-sidebar-body-item blue-orange-sidebar-body-item-active" : "blue-orange-sidebar-body-item"}>
 			<div
 				className="blue-orange-sidebar-body-item-left"
-				style={focused ? focusedStyle : defaultStyle}
-				onClick={itemClicked}>
+				style={focused ? focusedStyle : defaultStyle}>
 				{icon && <div className="blue-orange-sidebar-body-item-icon">{icon}</div>}
 				<div className="blue-orange-sidebar-body-item-label no-select">{label}</div>
 			</div>
-			<div className="blue-orange-sidebar-body-item-right">
+			<div ref={rightSideRef} className="blue-orange-sidebar-body-item-right">
 				{badge && (!hoverEffects || !isHovered) && <>{badge}</>}
 				{hoverItems && (hoverEffects && isHovered) && <>{hoverItems}</>}
 				{rightItems && (!hoverEffects || !isHovered) && <>{rightItems}</>}
